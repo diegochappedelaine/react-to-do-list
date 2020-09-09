bff8f605723a2edcdf78d4e0b574b19b9a691f2b
Diego Chappedelaine <diegochappedelaine@macbook-pro-de-diego.home>
2020-09-09 23:55:52 +0200

    notask updated

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Main from "./containers/Main";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-    </DndProvider>
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
   );
 }
 
